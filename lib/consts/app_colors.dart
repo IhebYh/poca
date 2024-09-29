3d5bc9095ed636b5ade2b687011093dab94ed3f7
IhebYh <iheb.yah@gmail.com>
2024-09-29 15:53:08 +0100

    Setup fonts + theme + main screen + create splash screen

---
+++ lib/consts/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xff6741FF);
+  static const lightBackground = Color(0xffF6F6F6);
+  static const darkBackground = Color(0xff242424);
+  static const grey = Color(0xff797979);
+  static const darkGrey = Color(0xff343434); 
+}
